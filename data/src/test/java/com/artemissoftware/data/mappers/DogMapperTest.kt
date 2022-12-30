d4591428a6532a2ab02fc45213bcf4c8e2b71f6f
gustavo <gustavo.isaac@gmail.com>
2022-12-30 11:11:31 +0000

    DogMapperTest

---
+++ data/src/test/java/com/artemissoftware/data/mappers/DogMapperTest.kt
@@ -0,0 +1,56 @@
+package com.artemissoftware.data.mappers
+
+import com.artemissoftware.data.remote.dto.BreedDto
+import com.artemissoftware.data.remote.dto.HeightDto
+import com.artemissoftware.data.remote.dto.ImageDto
+import com.artemissoftware.data.remote.dto.WeightDto
+import com.artemissoftware.domain.models.Breed
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DogMapperTest {
+
+    @Test
+    fun `map BreedDto to Breed`() {
+
+        val weightDto = WeightDto(
+            imperial = "6 - 13",
+            metric = "3 - 6"
+        )
+
+        val heightDto = HeightDto(
+            imperial = "9 - 11.5",
+            metric = "23 - 29"
+        )
+
+        val imageDto = ImageDto(
+            id = "BJa4kxc4X",
+            width = 1600,
+            height = 1199,
+            url = "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg"
+        )
+
+        val breedDto = BreedDto(
+            bredFor = "Small rodent hunting, lapdog",
+            breedGroup = "Toy",
+            heightDto = heightDto,
+            id = 1,
+            imageDto = imageDto,
+            lifeSpan = "10 - 12 years",
+            name = "Affenpinscher",
+            origin = "Germany, France",
+            referenceImageId = "BJa4kxc4X",
+            temperament = "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving",
+            weightDto = weightDto
+        )
+
+        val breed = Breed(
+            id = 1 ,
+            name = "Affenpinscher",
+            url = "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg"
+        )
+
+        assertEquals(breed, breedDto.toBreed())
+    }
+
+}
--- domain/src/main/java/com/artemissoftware/domain/models/Breed.kt
+++ domain/src/main/java/com/artemissoftware/domain/models/Breed.kt
@@ -4,4 +4,22 @@ data class Breed(
     val id: Int,
     val name: String,
     val url: String
-)
+){
+
+    companion object{
+
+        val mockBreeds = listOf(
+            Breed(
+                id = 1 ,
+                name = "Affenpinscher",
+                url = "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg"
+            ),
+            Breed(
+                id = 2,
+                name = "Saint Bernard",
+                url = "https://cdn2.thedogapi.com/images/_Qf9nfRzL.png"
+            ),
+        )
+    }
+
+}
