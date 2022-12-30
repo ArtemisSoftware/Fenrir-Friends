aeb3c58d228f11cde57e192ba46dd903b3955874
gustavo <gustavo.isaac@gmail.com>
2022-12-30 22:40:13 +0000

    FFCard

---
+++ core_ui/src/main/java/com/artemissoftware/core_ui/composables/card/FFCard.kt
@@ -0,0 +1,45 @@
+package com.artemissoftware.core_ui.composables.card
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.artemissoftware.core_ui.composables.text.FFText
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun FFCard(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    elevation: Dp = 12.dp,
+    shape: Shape = RoundedCornerShape(4.dp),
+    content: @Composable () -> Unit
+) {
+
+    Card(
+        modifier = modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        onClick = onClick,
+        elevation = elevation,
+        shape = shape,
+        content = content
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FFCardPreview() {
+
+    FFCard{
+        FFText(text = "Fenrir Friends")
+    }
+
+}
