c6473e5c214783e911e63b87d2185fdfa0ada213
gustavo <gustavo.isaac@gmail.com>
2022-12-31 14:18:33 +0000

    GalleryList

---
+++ app/src/main/java/com/artemissoftware/fenrirfriends/screen/gallery/composables/GalleryList.kt
@@ -0,0 +1,36 @@
+package com.artemissoftware.fenrirfriends.screen.gallery.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.artemissoftware.fenrirfriends.screen.gallery.GalleryEvents
+import com.artemissoftware.fenrirfriends.screen.gallery.GalleryState
+
+@Composable
+private fun GalleryList(
+    state: GalleryState,
+    events: ((GalleryEvents) -> Unit)? = null
+){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        items(state.breeds) { breed->
+
+            BreedCard(
+                breed = breed,
+                onClick = {
+                    events?.invoke(GalleryEvents.GoToBreedDetail(breed))
+                }
+            )
+        }
+    }
+}
