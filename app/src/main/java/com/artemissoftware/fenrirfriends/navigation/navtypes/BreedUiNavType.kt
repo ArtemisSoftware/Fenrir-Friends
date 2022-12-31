01de86e3130b0d6d1874c9b4cb92b40990fbfd34
gustavo <gustavo.isaac@gmail.com>
2022-12-31 12:07:27 +0000

    BreedUiNavType

---
+++ app/src/main/java/com/artemissoftware/fenrirfriends/navigation/navtypes/BreedUiNavType.kt
@@ -0,0 +1,24 @@
+package com.artemissoftware.fenrirfriends.navigation.navtypes
+
+import android.os.Build
+import android.os.Bundle
+import androidx.navigation.NavType
+import com.artemissoftware.fenrirfriends.screen.models.BreedUi
+import com.google.gson.Gson
+
+class BreedUiNavType : NavType<BreedUi>(isNullableAllowed = true) {
+    override fun get(bundle: Bundle, key: String): BreedUi? {
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            bundle.getParcelable(key, BreedUi::class.java)
+        } else {
+            bundle.getParcelable(key)
+        }
+    }
+    override fun parseValue(value: String): BreedUi {
+        return Gson().fromJson(value, BreedUi::class.java)
+    }
+    override fun put(bundle: Bundle, key: String, value: BreedUi) {
+        bundle.putParcelable(key, value)
+    }
+}
