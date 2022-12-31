635b6ff37a3be2dc6c438246a57003058b1039f7
gustavo <gustavo.isaac@gmail.com>
2022-12-31 12:22:06 +0000

    FenrirFriendsApplication

---
+++ app/src/main/java/com/artemissoftware/fenrirfriends/FenrirFriendsApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.fenrirfriends
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FenrirFriendsApplication : Application()
