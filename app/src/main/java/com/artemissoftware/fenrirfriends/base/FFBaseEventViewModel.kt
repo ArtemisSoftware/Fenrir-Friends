e0a8405c34d424f9479cafa03ecb7dca8a5b621b
gustavo <gustavo.isaac@gmail.com>
2022-12-31 10:14:06 +0000

    FFBaseEventViewModel

---
+++ app/src/main/java/com/artemissoftware/fenrirfriends/base/FFBaseEventViewModel.kt
@@ -0,0 +1,23 @@
+package com.artemissoftware.fenrirfriends.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.artemissoftware.fenrirfriends.base.events.FFBaseEvents
+import com.artemissoftware.fenrirfriends.base.events.UiEvent
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+abstract class FFBaseEventViewModel <E: FFBaseEvents> : ViewModel() {
+
+    private val _uiEvent =  Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    open fun onTriggerEvent(event: E) {}
+
+    protected fun sendUiEvent(event: UiEvent) {
+        viewModelScope.launch {
+            _uiEvent.send(event)
+        }
+    }
+}
