c6712e26ab6a83844313fa5dd6c3ed23bfcf433b
gustavo <gustavo.isaac@gmail.com>
2022-12-31 14:22:26 +0000

    GalleryRoute

---
+++ app/src/main/java/com/artemissoftware/fenrirfriends/screen/gallery/GalleryRoute.kt
@@ -0,0 +1,17 @@
+package com.artemissoftware.fenrirfriends.screen.gallery
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.artemissoftware.fenrirfriends.navigation.DestinationRoutes
+import com.artemissoftware.fenrirfriends.navigation.NavigationRoute
+
+object GalleryRoute: NavigationRoute<GalleryEvents, GalleryViewModel> {
+
+    override fun getDestination() = DestinationRoutes.HomeGraph.gallery
+
+    @Composable
+    override fun viewModel(): GalleryViewModel = hiltViewModel()
+
+    @Composable
+    override fun Content(viewModel: GalleryViewModel) = GalleryScreen(viewModel = viewModel)
+}
