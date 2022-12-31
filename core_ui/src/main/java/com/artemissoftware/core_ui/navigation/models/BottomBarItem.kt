7b2bebc70f249e0b96938a3daad23f0281fe1fc5
gustavo <gustavo.isaac@gmail.com>
2022-12-31 08:53:49 +0000

    BottomBarItem

---
+++ core_ui/src/main/java/com/artemissoftware/core_ui/navigation/models/BottomBarItem.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.core_ui.navigation.models
+
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomBarItem(@StringRes val title: Int,
+                         val activeIcon: ImageVector,
+                         val inactiveIcon: ImageVector,
+                         val destination: BaseDestination)
