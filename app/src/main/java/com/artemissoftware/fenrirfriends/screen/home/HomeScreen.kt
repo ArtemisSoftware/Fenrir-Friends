f3ba136bf911e3defc1941e0dc63a8bad11b7478
gustavo <gustavo.isaac@gmail.com>
2022-12-31 15:16:32 +0000

    HomeScreen

---
+++ app/src/main/java/com/artemissoftware/fenrirfriends/screen/home/HomeScreen.kt
@@ -0,0 +1,22 @@
+package com.artemissoftware.fenrirfriends.screen.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.artemissoftware.core_ui.composables.scaffold.FFScaffold
+import com.artemissoftware.core_ui.composables.scaffold.FFUiScaffoldState
+import com.artemissoftware.fenrirfriends.navigation.graphs.HomeNavigationGraph
+
+@Composable
+fun HomeScreen(
+    navController: NavHostController = rememberNavController(),
+    scaffoldState: FFUiScaffoldState
+) {
+    FFScaffold(
+        ffUiScaffoldState = scaffoldState,
+        bottomBarItems = scaffoldState.bottomBarDestinations.value,
+        navController = navController
+    ) {
+        HomeNavigationGraph(navController = navController, scaffoldState = scaffoldState)
+    }
+}
