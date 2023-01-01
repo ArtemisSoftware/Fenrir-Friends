2b807d0de1540b036984087ac8c66edbb37ce1df
gustavo <gustavo.isaac@gmail.com>
2023-01-01 19:06:36 +0000

    Added new table

--- data/src/main/java/com/artemissoftware/data/dabase/FenrirDatabase.kt
+++ data/src/main/java/com/artemissoftware/data/dabase/FenrirDatabase.kt
@@ -3,14 +3,17 @@ package com.artemissoftware.data.dabase
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.artemissoftware.data.dabase.dao.BreedDao
+import com.artemissoftware.data.dabase.dao.BreedRemoteKeysDao
 import com.artemissoftware.data.dabase.entities.BreedEntity
+import com.artemissoftware.data.dabase.entities.BreedRemoteKeysEntity
 
 
 @Database(
-    entities = [BreedEntity::class],
+    entities = [BreedEntity::class, BreedRemoteKeysEntity::class],
     version = 1
 )
 abstract class FenrirDatabase: RoomDatabase() {
 
     abstract val breedDao: BreedDao
+    abstract val breedRemoteKeysDao: BreedRemoteKeysDao
 }
