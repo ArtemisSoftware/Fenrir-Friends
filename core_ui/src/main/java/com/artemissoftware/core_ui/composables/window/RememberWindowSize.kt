fa3190737ea34dbd22963d63e07546027dea02ae
gustavo <gustavo.isaac@gmail.com>
2023-01-06 09:33:48 +0000

    rememberWindowSize

---
+++ core_ui/src/main/java/com/artemissoftware/core_ui/composables/window/RememberWindowSize.kt
@@ -0,0 +1,37 @@
+package com.artemissoftware.core_ui.composables.window
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalConfiguration
+import com.artemissoftware.core_ui.composables.window.models.WindowSize
+import com.artemissoftware.core_ui.composables.window.models.WindowType
+
+@Composable
+fun rememberWindowSize(): WindowSize {
+    val configuration = LocalConfiguration.current
+    val screenWidth by remember(key1 = configuration) {
+        mutableStateOf(configuration.screenWidthDp)
+    }
+    val screenHeight by remember(key1 = configuration) {
+        mutableStateOf(configuration.screenHeightDp)
+    }
+
+    return WindowSize(
+        width = getScreenWidth(screenWidth),
+        height = getScreenHeight(screenHeight)
+    )
+}
+
+fun getScreenWidth(width: Int): WindowType = when {
+    width < 600 -> WindowType.Compact
+    width < 840 -> WindowType.Medium
+    else -> WindowType.Expanded
+}
+
+fun getScreenHeight(height: Int): WindowType = when {
+    height < 480 -> WindowType.Compact
+    height < 900 -> WindowType.Medium
+    else -> WindowType.Expanded
+}
